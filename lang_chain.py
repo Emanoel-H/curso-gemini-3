dc9d7e0db4e7eefd1f4b2598604c7b547f6d558e
Emanoel <emanoel.pluto@gmail.com>
2026-08-14 17:14:13 -0300

    using ChatPromptTemplate instead

--- lang_chain.py
+++ lang_chain.py
@@ -5,6 +5,7 @@ from my_keys import GEMINI_API_KEY
 from my_helper import encode_image
 from pathlib import Path
 from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 llm = ChatGoogleGenerativeAI(
     api_key= GEMINI_API_KEY,
@@ -15,21 +16,51 @@ path_image = Path.home() / "Desktop" / "curso_gemini_3" / "images" / "exemplo_gr
 
 image_64 = encode_image(path_image)
 
-pergunta = "Descreva a imagem: "
+# pergunta = "Descreva a imagem: "
 
-mensagem = HumanMessage(
-    content= [
+template_analisador = ChatPromptTemplate.from_messages(
+    [
+        ("system", """
+        Assuma que você é um analisador de imagens. A sua tarefa principal
+        consiste em analisar uma imagem e extrair informações importantes 
+        de forma objetiva.
+        
+        # FORMATO DE SAÍDA
+        Descrição da Imagem: 'Coloque a sua descrição da imagem aqui'
+        Rótulos: 'Coloque uma lista com três termos chave separados por vírgula'
+                
+        """),
+        ("user",
+         [
         {
             "type" : "text",
-            "text" : pergunta
+            "text" : "Descreva a imagem: "
         },
         {
             "type" : "image_url",
-            "image_url" : f"data:image/jpeg;base64,{image_64}"
+            "image_url" : {"url":"data:image/jpeg;base64,{image_64}"}
         }
-    ],
+    ])
+    ]
 )
 
-resposta = llm.invoke([mensagem])
+cadeia = template_analisador | llm | StrOutputParser()
 
+resposta = cadeia.invoke({"image_64": image_64})
 print(resposta)
+
+# mensagem = HumanMessage(
+#     content= [
+#         {
+#             "type" : "text",
+#             "text" : pergunta
+#         },
+#         {
+#             "type" : "image_url",
+#             "image_url" : f"data:image/jpeg;base64,{image_64}"
+#         }
+#     ]
+# )
+
+# resposta = llm.invoke([mensagem])
+
