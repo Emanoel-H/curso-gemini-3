e16be4e5fbb0b5260f99832dc09ead0dad377f63
Emanoel <emanoel.pluto@gmail.com>
2026-08-15 08:25:45 -0300

    creating class ImageAnalysisTool

---
+++ image_analysis_tool.py
@@ -0,0 +1,15 @@
+from langchain.tools import BaseTool
+
+class ImageAnalysisTool(BaseTool):
+    name:str = "ImageAnalysisTool"
+    description:str = """
+    Utilize esta ferramenta sempre que for solicitado que você faça uma análise de imagem.
+    
+    # Entradas Requeridas
+    - 'nome_imagem' (str) : Nome da imagem a ser analisada com extensão de JPG.
+    Exemplo: teste.jpg ou teste.jpeg 
+    """
+    return_direct : bool = False
+
+    def _run(self, acao):
+        return ""
