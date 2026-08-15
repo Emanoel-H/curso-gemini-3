ce3f003c1cbd39d2a1724fa85bed7beca366ee9e
Emanoel <emanoel.pluto@gmail.com>
2026-08-15 16:37:49 -0300

    calling llm using langchain

--- connecting_to_llm.py
+++ connecting_to_llm.py
@@ -1,4 +1,21 @@
+from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from my_models import GEMINI_FLASH
 from my_keys import GEMINI_API_KEY
+
+llm = ChatGoogleGenerativeAI(
+            api_key=GEMINI_API_KEY,
+            model=GEMINI_FLASH
+        )
+
+prompt_model = PromptTemplate(
+    template="Me diga quais os impactos da IA no assunto {assunto}",
+    input_variables=["assunto"]
+)
+
+chain = prompt_model | llm | StrOutputParser()
+
+resposta = chain.invoke({"assunto": "Agricultura"})
+
+print(resposta)
