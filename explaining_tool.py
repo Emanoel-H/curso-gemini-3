38fec1756d9bab47cb0814a1962260b536929b8b
Emanoel <emanoel.pluto@gmail.com>
2026-08-15 15:37:35 -0300

    fixing import at explaining_tool.py

--- explaining_tool.py
+++ explaining_tool.py
@@ -1,8 +1,7 @@
-from lang_chain import cadeia
 from langchain.tools import BaseTool
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.chat_models import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from my_models import GEMINI_FLASH
 from my_keys import GEMINI_API_KEY
 import ast
